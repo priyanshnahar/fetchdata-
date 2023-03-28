be64e844f43a87c879de0f42acb81ada4de531d9
Priyansh Nahar <priyanshnahar1998@gmail.com>
2023-03-28 13:31:45 +0530

    Initial commit

---
+++ datafetch/src/App.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const result = await axios.get('https://jsonplaceholder.typicode.com/posts');
+      setPosts(result.data);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {posts.map(post => (
+        <div key={post.id}>
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
---
+++ datafetch/src/a.js
@@ -0,0 +1,10 @@
+function a (){
+    let x = "world";
+    console.log("hello");
+    function b (){
+        console.log(x);
+    }
+    b()
+}
+
+a();
